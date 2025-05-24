4355093e8a28c99f75e1111485fca984660e4781
Josko de Boer <Josko.deboer@xomnia.com>
2025-05-24 02:23:47 +0200

    if it works don't fix it.

--- run-marp-slides.sh
+++ run-marp-slides.sh
@@ -9,14 +9,12 @@ for file in presentation/*.md; do
   docker run --rm --init \
     -v "$PWD:/home/marp/app/" \
     -e LANG=$LANG \
-    -e MARP_USER="$(id -u):$(id -g)" \
     marpteam/marp-cli \
     "$file" --pdf --allow-local-files --output "tmp/presentation/dist/$name.pdf"
 
   docker run --rm --init \
     -v "$PWD:/home/marp/app/" \
     -e LANG=$LANG \
-    -e MARP_USER="$(id -u):$(id -g)" \
     marpteam/marp-cli \
     "$file" --pptx --allow-local-files --output "tmp/presentation/dist/$name.pptx"
 done
