d377f1b658101364f9aae88ce0b6ec0c2ce1e397
Josko de Boer <Josko.deboer@xomnia.com>
2025-05-24 02:11:26 +0200

    Minor Update

--- run-marp-slides.sh
+++ run-marp-slides.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 for file in presentation/*.md; do
   name=$(basename "$file" .md)
-  marp "$file" --pdf --allow-local-files --output "presentation/dist/$name.pdf"
-  marp "$file" --pptx --allow-local-files --output "presentation/dist/$name.pptx"
+  echo "Building: $file"
+  marp "$file" --pdf --allow-local-files --output "tmp/presentation/dist/$name.pdf"
+  marp "$file" --pptx --allow-local-files --output "tmp/presentation/dist/$name.pptx"
 done
