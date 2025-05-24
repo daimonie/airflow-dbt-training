aa46725b1100c10c80b15405619b6084d93255db
Josko de Boer <Josko.deboer@xomnia.com>
2025-05-24 02:16:52 +0200

    instructed about the docs

--- run-marp-slides.sh
+++ run-marp-slides.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
+mkdir -p tmp/presentation/dist
 for file in presentation/*.md; do
   name=$(basename "$file" .md)
-  echo "Processing $file"
-  marp "$file" --pdf --allow-local-files --output "tmp/presentation/dist/$name.pdf"
-  marp "$file" --pptx --allow-local-files --output "tmp/presentation/dist/$name.pptx"
+  echo "Building $file -> $name.[pdf|pptx]"
+  
+  docker run --rm --init \
+    -v "$PWD:/home/marp/app/" \
+    -e LANG=$LANG \
+    marpteam/marp-cli \
+    "$file" --pdf --allow-local-files --output "tmp/presentation/dist/$name.pdf"
+
+  docker run --rm --init \
+    -v "$PWD:/home/marp/app/" \
+    -e LANG=$LANG \
+    marpteam/marp-cli \
+    "$file" --pptx --allow-local-files --output "tmp/presentation/dist/$name.pptx"
 done
